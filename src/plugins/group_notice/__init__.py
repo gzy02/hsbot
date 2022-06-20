a75bf7bfaca7b011e396466bdc1fc8e476bad63c
gzy02 <1262454489@qq.com>
2022-06-20 18:51:15 +0800

    欢迎入群

--- src/plugins/group_notice/__init__.py
+++ src/plugins/group_notice/__init__.py
@@ -1,6 +1,7 @@
 
 from nonebot import on_notice
 from nonebot.adapters.onebot.v11 import Bot,RequestEvent,FriendRequestEvent,GroupRequestEvent
+from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 
 """
 [notice.group_increase.approve]: {
@@ -14,4 +15,33 @@ from nonebot.adapters.onebot.v11 import Bot,RequestEvent,FriendRequestEvent,Grou
     'operator_id': 0
 }
 """
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Author         : yanyongyu
+@Date           : 2020-09-18 00:00:13
+@LastEditors    : yanyongyu
+@LastEditTime   : 2021-01-01 17:46:15
+@Description    : None
+@GitHub         : https://github.com/yanyongyu
+"""
+__author__ = "yanyongyu"
+
+from nonebot.typing import T_State
+from nonebot.matcher import Matcher
+from nonebot.adapters import Bot, Event
+from nonebot.permission import SUPERUSER
+from nonebot import get_driver, on_command, on_notice, on_message
+from nonebot.adapters.onebot.v11 import Bot,GroupIncreaseNoticeEvent
+
+
+async def _group_increase(bot: Bot, event: Event) -> bool:
+    return isinstance(event, GroupIncreaseNoticeEvent)
+
+
+group_increase = on_notice(_group_increase, priority=10, block=True)
+@group_increase.handle()
+async def _group_increase(bot: Bot, event: GroupIncreaseNoticeEvent):
+    return await group_increase.send(MessageSegment.at(event.get_user_id())+"欢迎入群")
 
