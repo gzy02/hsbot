1d16aced7e76857c9a77a44c18a383c7ac3c5331
gzy02 <1262454489@qq.com>
2022-06-20 18:44:25 +0800

    进入群聊

--- src/plugins/add_friend/__init__.py
+++ src/plugins/add_friend/__init__.py
@@ -1,6 +1,7 @@
 
 from nonebot import on_request
 from nonebot.adapters.onebot.v11 import Bot,RequestEvent,FriendRequestEvent,GroupRequestEvent
+from nonebot.log import logger
 
 """
 [request.friend]: {'time': 1655710128, 'self_id': 2457256025, 'post_type': 'request', 'request_type': 'friend', 'user_id': 1262454489, 'comment': 'gzy', 'flag': '1655710202000000'}
@@ -24,4 +25,7 @@ add_group=on_request(_is_add_group,block=False,priority=2)
 @add_group.handle()
 async def _add_group(bot: Bot, event: GroupRequestEvent):
     print(event.get_event_description())
-    return await event.approve(bot)
+    try:
+        await event.approve(bot)
+    except:
+        logger.info('已经进群了')
