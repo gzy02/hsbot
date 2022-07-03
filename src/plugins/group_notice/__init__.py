00439303039a07047d18e0269bfd18c0f25bc446
gzy02 <1262454489@qq.com>
2022-07-03 14:26:03 +0800

    notice

--- src/plugins/group_notice/__init__.py
+++ src/plugins/group_notice/__init__.py
@@ -1,37 +1,8 @@
 
 from nonebot import on_notice
-from nonebot.adapters.onebot.v11 import Bot,RequestEvent,FriendRequestEvent,GroupRequestEvent
+from nonebot.adapters.onebot.v11 import Bot,Event,RequestEvent,FriendRequestEvent,GroupRequestEvent
 from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 
-"""
-[notice.group_increase.approve]: {
-    'time': 1655716407, 
-    'self_id': 2149627292, 
-    'post_type': 'notice', 
-    'notice_type': 'group_increase', 
-    'sub_type': 'approve', 
-    'user_id': 2149627292, 
-    'group_id': 649655353, 
-    'operator_id': 0
-}
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-@Author         : yanyongyu
-@Date           : 2020-09-18 00:00:13
-@LastEditors    : yanyongyu
-@LastEditTime   : 2021-01-01 17:46:15
-@Description    : None
-@GitHub         : https://github.com/yanyongyu
-"""
-__author__ = "yanyongyu"
-
-from nonebot.typing import T_State
-from nonebot.matcher import Matcher
-from nonebot.adapters import Bot, Event
-from nonebot.permission import SUPERUSER
 from nonebot import get_driver, on_command, on_notice, on_message
 from nonebot.adapters.onebot.v11 import Bot,GroupIncreaseNoticeEvent
 
