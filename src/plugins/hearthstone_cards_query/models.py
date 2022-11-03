2fef593abf016e90f50d8f7884ae8827b68cf48e
gzy02 <1262454489@qq.com>
2022-11-03 14:21:48 +0800

    model

--- src/plugins/hearthstone_cards_query/models.py
+++ src/plugins/hearthstone_cards_query/models.py
@@ -21,23 +21,27 @@ class Cards(BaseModel):
                             index=True)  #构造及解析卡牌代码的数值,dbfId
     id = pw.CharField(max_length=32, null=False, unique=True)  # 卡牌的字符串标识
     name = pw.CharField(max_length=32, null=False, index=True)
-    #随从牌、法术牌、英雄牌、武器牌
-    type = pw.CharField(max_length=32,
-                        constraints=[pw.Check('type in ("MINION", "SPELL", "HERO","WEAPON")')])
+    #随从牌、法术牌、英雄牌、武器牌、地标
+    type = pw.CharField(
+        max_length=32
+        #constraints=[pw.Check('type in ("MINION", "SPELL", "HERO","WEAPON","LOCATION")')]
+    )
     #普通、稀有、史诗、传说、免费
     rarity = pw.CharField(
         max_length=32,
-        null=False,
-        constraints=[pw.Check('rarity in ("COMMON", "RARE", "EPIC", "LEGENDARY","FREE")')])
+        null=False
+        #constraints=[pw.Check('rarity in ("COMMON", "RARE", "EPIC", "LEGENDARY","FREE")')]
+    )
     #中立及十职业
     cardClass = pw.CharField(
         max_length=32,
-        null=False,
-        constraints=[
-            pw.Check(
-                'cardClass in ("MAGE", "HUNTER", "PRIEST", "NEUTRAL", "WARLOCK", "ROGUE", "DRUID", "SHAMAN", "WARRIOR", "PALADIN", "DEMONHUNTER")'
-            )
-        ])
+        null=False
+        #constraints=[
+        #    pw.Check(
+        #        'cardClass in ("MAGE", "HUNTER", "PRIEST", "NEUTRAL", "WARLOCK", "ROGUE", "DRUID", "SHAMAN", "WARRIOR", "PALADIN", "DEMONHUNTER")'
+        #    )
+        #]
+    )
     set = pw.CharField(max_length=32, null=False)  #来自哪个系列或拓展包
     text = pw.CharField(max_length=1024)  #卡牌描述
     artist = pw.CharField(max_length=255)  #画师
@@ -55,12 +59,13 @@ class MINIONCards(BaseModel):
     #随从种族，可为空
     #龙、恶魔、海盗、野兽、图腾、鱼人、元素、机械、野猪人、娜迦、全部
     race = pw.CharField(
-        max_length=32,
-        constraints=[
-            pw.Check(
-                'race in ("","NAGA","DRAGON", "DEMON", "PIRATE", "BEAST", "TOTEM", "MURLOC", "ELEMENTAL", "MECHANICAL", "QUILBOAR", "ALL")'
-            )
-        ])
+        max_length=32
+        #constraints=[
+        #    pw.Check(
+        #        'race in ("","NAGA","DRAGON", "DEMON", "PIRATE", "BEAST", "TOTEM", "MURLOC", "ELEMENTAL", "MECHANICAL", "QUILBOAR", "ALL")'
+        #    )
+        #]
+    )
 
 
 class SPELLCards(BaseModel):
@@ -70,11 +75,12 @@ class SPELLCards(BaseModel):
     #法术派系，可为空
     #火焰、奥术、神圣、暗影、邪能、自然、冰霜
     spellSchool = pw.CharField(
-        max_length=32,
-        constraints=[
-            pw.Check(
-                'spellSchool in ("","FIRE", "ARCANE", "HOLY", "SHADOW", "FEL", "NATURE", "FROST")')
-        ])
+        max_length=32
+        #constraints=[
+        #    pw.Check(
+        #        'spellSchool in ("","FIRE", "ARCANE", "HOLY", "SHADOW", "FEL", "NATURE", "FROST")')
+        #]
+    )
 
 
 class HEROCards(BaseModel):
@@ -90,3 +96,9 @@ class WEAPONCards(BaseModel):
     attack = pw.IntegerField(null=False, constraints=[pw.Check('attack >= 0')])
     #耐久度
     durability = pw.IntegerField(null=False, constraints=[pw.Check('durability >= 0')])
+
+
+class LOCATIONCards(BaseModel):
+    Cardid = pw.ForeignKeyField(Cards, primary_key=True, null=False, unique=True, index=True)
+    cost = pw.IntegerField(null=False)  #法力值消耗(费用)
+    health = pw.IntegerField()  #生命
